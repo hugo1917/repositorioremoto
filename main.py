7b86facb7e2f75a1613b6ef4c8b06fd4522a8226
hugo19 <hfraid2@gmail.com>
2026-04-27 16:27:16 -0600

    Se integra main.py con llamadas a funciones

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from funciones import suma, resta
+from funciones import suma, resta, multiplicacion
 
 print(f"La suma es: {suma(10, 5)}")
 print(f"La resta es: {resta(10, 5)}")
+print(f"La multiplicacion es: {multiplicacion(10,5)}")
